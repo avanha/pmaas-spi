5dd4283efe586717f7cdfad89f593f29fd9f9359
Andre Vanha <andre@vanhas.com>
2022-09-29 09:26:05 -0600

    Added getTemplate function to container.

--- container.go
+++ container.go
@@ -2,7 +2,10 @@ package spi
 
 import "net/http"
 
+type ITemplate interface {}
+
 type IPMAASContainer interface {
 	AddRoute(path string, handlerFunc http.HandlerFunc)
 	RenderList(w http.ResponseWriter, r *http.Request, items []interface{})
+	GetTemplate(path string) (ITemplate, error)
 }
