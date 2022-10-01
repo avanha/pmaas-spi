e338db8cb7e2dc5148663fd514a2f8837a19a0ce
Andre Vanha <andre@vanhas.com>
2022-09-30 22:01:43 -0600

    Work on entity renderer

--- renderPlugin.go
+++ renderPlugin.go
@@ -1,8 +1,13 @@
 package spi
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 type IPMAASRenderPlugin interface {
 	IPMAASPlugin
 	RenderList(w http.ResponseWriter, r *http.Request, items []interface{})
+	RenderEntity(w io.Writer, item any)
 }
+
