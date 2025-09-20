ef1fa7e4380adde84c376b9ee2ba9267adb54fa6
Andre Vanha <andre@vanhas.com>
2025-09-20 17:56:06 -0600

    Add comment on IPMAASPlugin interface.

--- plugin.go
+++ plugin.go
@@ -1,5 +1,7 @@
 package spi
 
+// IPMAASPlugin is the interface all plugins must implement.
+// The core will call Init, Start and Stop in sequence, all from the same go routine.
 type IPMAASPlugin interface {
 	Init(container IPMAASContainer)
 	Start()
