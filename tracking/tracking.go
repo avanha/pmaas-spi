57f560af4ec26b07449d592cbb3e005365965ebd
Andre Vanha <andre@vanhas.com>
2025-11-15 01:23:33 -0700

    Fix warnings in spi/tracking

--- tracking/tracking.go
+++ tracking/tracking.go
@@ -6,23 +6,27 @@ import (
 	"pmaas.io/spi"
 )
 
-const TRACKING_MODE_POLL = 1
-const TRACKING_MODE_PUSH = 2
+const ModePoll = 1
+const ModePush = 2
 
-type TrackingConfig struct {
+type Config struct {
 	Name         string
 	TrackingMode int
 	Schema       string
 }
 
+func (tc *Config) Clone() Config {
+	return *tc
+}
+
 type Trackable interface {
-	TrackingConfig() TrackingConfig
+	TrackingConfig() Config
 	Data() any
 }
 
 var TrackableType = reflect.TypeOf((*Trackable)(nil)).Elem()
 
-func NewTrackableStub(container spi.IPMAASContainer, pmaasId string) Trackable {
+func CreateTrackableStub(container spi.IPMAASContainer, pmaasId string) Trackable {
 	invoker := func(f func(trackable Trackable)) error {
 		return container.InvokeOnEntity(pmaasId, func(entity any) {
 			f(entity.(Trackable))
@@ -39,8 +43,8 @@ type trackableStub struct {
 	invokeFn      func(func(trackable Trackable)) error
 }
 
-func (t *trackableStub) TrackingConfig() TrackingConfig {
-	resultCh := make(chan TrackingConfig)
+func (t *trackableStub) TrackingConfig() Config {
+	resultCh := make(chan Config)
 	err := t.invokeFn(func(instance Trackable) {
 		defer func() { close(resultCh) }()
 		resultCh <- instance.TrackingConfig()
