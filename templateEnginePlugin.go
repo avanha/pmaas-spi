df6ff0d650425e0bbd2263ddf3efca50b835efb1
Andre Vanha <andre@vanhas.com>
2026-01-22 11:23:04 -0700

    Created a TemplateBasedRendererFactory function to replace the repeated code found in all plugin https handlers.
    1. Loads the template
    2. Declares a function that verifies the incoming item type, and evaluates the template using the type.
    3.  Returns the function, and any template styles and scripts.

--- templateEnginePlugin.go
+++ templateEnginePlugin.go
@@ -1,6 +1,7 @@
 package spi
 
 import (
+	"fmt"
 	"io"
 	"io/fs"
 	"text/template"
@@ -28,4 +29,38 @@ type CompiledTemplate struct {
 type IPMAASTemplateEnginePlugin interface {
 	IPMAASPlugin
 	GetTemplate(templateInfo *TemplateInfo) (CompiledTemplate, error)
+}
+
+func TemplateBasedRendererFactory(
+	container IPMAASContainer,
+	templateInfo *TemplateInfo,
+	typeCheckFn func(any) bool,
+	typeName string) (EntityRenderer, error) {
+
+	// Load the template
+	compiledTemplate, err := container.GetTemplate(templateInfo)
+
+	if err != nil {
+		return EntityRenderer{},
+			fmt.Errorf("unable to load %s template: %w", templateInfo.Name, err)
+	}
+
+	// Declare a function that casts the entity to the expected type and evaluates it via the template loaded above
+	renderer := func(w io.Writer, entity any) error {
+		if ok := typeCheckFn(entity); !ok {
+			return fmt.Errorf("item is not an instance of %s", typeName)
+		}
+
+		if err := compiledTemplate.Instance.Execute(w, entity); err != nil {
+			return fmt.Errorf("unable to execute %s template: %w", templateInfo.Name, err)
+		}
+
+		return nil
+	}
+
+	return EntityRenderer{
+		StreamingRenderFunc: renderer,
+		Styles:              compiledTemplate.Styles,
+		Scripts:             compiledTemplate.Scripts,
+	}, nil
 }
