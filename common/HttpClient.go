a331ff0deb6e6cabbd3052d0e6310a12c0b6528b
Andre Vanha <andre@vanhas.com>
2026-03-27 23:45:43 -0600

    Added Post method to the HttpClient interface and the default implementation.

--- common/HttpClient.go
+++ common/HttpClient.go
@@ -1,14 +1,22 @@
 package common
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+)
 
 // HttpClient is an interface for making HTTP Requests.
 type HttpClient interface {
 	Get(url string) (*http.Response, error)
+	Post(uri string, contentType string, body *bytes.Reader) (*http.Response, error)
 }
 
 type DefaultHttpClient struct{}
 
 func (client *DefaultHttpClient) Get(url string) (*http.Response, error) {
 	return http.Get(url)
+}
+
+func (client *DefaultHttpClient) Post(uri string, contentType string, body *bytes.Reader) (*http.Response, error) {
+	return http.Post(uri, contentType, body)
 }
